a30150c89124c60fd6db601df202971c29ef5b22
malikabuhammad <71324950+malikabuhammad@users.noreply.github.com>
2023-03-04 17:37:41 +0200

    Styling FAQ Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,9 @@
+import {About}from '../styles'
 
-
+import styled from 'styled-components';
 const FaqSection = ()=>{
     return (
-    <div className="faq">
+    <Faq>
         <h2>Any Questions <span>FAQ</span></h2>
         <div className="question">
             <h4>How Do I Start </h4>
@@ -11,6 +12,7 @@ const FaqSection = ()=>{
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, vitae?</p>
 
             </div>
+            <div className="faq-line"></div>
         </div>
         <div className="question">
             <h4>Daily Schedule </h4>
@@ -19,6 +21,8 @@ const FaqSection = ()=>{
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, vitae?</p>
                 
             </div>
+            <div className="faq-line"></div>
+
         </div>
         <div className="question">
             <h4>Diffreent Payment Methods </h4>
@@ -27,6 +31,8 @@ const FaqSection = ()=>{
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, vitae?</p>
                 
             </div>
+            <div className="faq-line"></div>
+
         </div>
         <div className="question">
             <h4>What Product do you offer  </h4>
@@ -35,9 +41,36 @@ const FaqSection = ()=>{
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, vitae?</p>
                 
             </div>
+            <div className="faq-line"></div>
+
         </div>
         
-    </div>
+    </Faq>
     )
 }
+
+const Faq=styled(About)`
+display: block;
+span{
+    display: block;
+}
+h2 { 
+    padding-bottom: 2rem;
+    font-weight: lighter;
+}
+.faq-line { 
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+
+}
+.question {
+    padding: 2rem  0rem;
+}
+p {
+    padding: 1rem 0rem;
+}
+
+`
 export default FaqSection;
